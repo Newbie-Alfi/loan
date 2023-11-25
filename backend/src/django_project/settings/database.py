9db0c53c7ff4f428f94d485576de76bd46722743
Newbie-Alfi <aleksandr20021008@gmail.com>
2023-11-25 13:45:29 +0300

    add docker

--- backend/src/django_project/settings/database.py
+++ backend/src/django_project/settings/database.py
@@ -1,11 +1,13 @@
+import os
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config["database"]["name"],
-        "USER": config["database"]["user"],
-        "PASSWORD": config["database"]["password"],
-        "HOST": config["database"]["host"],
-        "PORT": config["database"]["port"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
         "ATOMIC_REQUESTS": True,
     }
 }
